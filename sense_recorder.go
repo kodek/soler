3f0b1a016bcb3607742b1d3aa15135d2bd8aa0a4
Hernan Cosi <hernan@hernancosi.com>
2018-09-05 18:38:45 -0700

    Try a different backoff library for Sense reconnections.

--- sense_recorder.go
+++ sense_recorder.go
@@ -1,16 +1,17 @@
 package soler
 
 import (
-	"errors"
 	"time"
 
-	"github.com/cenkalti/backoff"
+	"github.com/jpillora/backoff"
+
 	"github.com/golang/glog"
 	sense "github.com/kodek/sense-api"
 )
 
 type SenseRecorder struct {
-	Db *Database
+	Db             *Database
+	backoffTracker *backoff.Backoff
 }
 
 func (rec *SenseRecorder) StartAndLoop(config Sense) {
@@ -19,22 +20,31 @@ func (rec *SenseRecorder) StartAndLoop(config Sense) {
 		glog.Fatal("Cannot authenticate to Sense. ", err)
 	}
 
-	operation := func() error {
-		rec.recordIndefinitely(c)
-		glog.Error("Reconnecting...")
-		return errors.New("Retryable error")
+	backoffTracker := backoff.Backoff{
+		Min:    1 * time.Second,
+		Max:    15 * time.Minute,
+		Factor: 1.5,
+		Jitter: true,
 	}
-	err = backoff.Retry(operation, backoff.NewExponentialBackOff())
-	if err != nil {
-		glog.Fatal(err)
+
+	// Run indefinitely. Reconnect with exponential backoff if needed.
+	for {
+		rec.recordIndefinitely(c, &backoffTracker)
+
+		throttleTime := backoffTracker.Duration()
+		glog.Error("Reconnecting in ", throttleTime.String())
+		time.Sleep(throttleTime)
 	}
 }
 
-func (rec *SenseRecorder) recordIndefinitely(client sense.Client) {
+func (rec *SenseRecorder) recordIndefinitely(client sense.Client, backoffTracker *backoff.Backoff) {
 	recv := rec.connectOrDie(client)
 
 	throttler := time.NewTicker(2 * time.Second)
 	for response := range recv {
+		// Successful communication, so reset the backoff tracker.
+		backoffTracker.Reset()
+
 		err := rec.Db.AddSenseRealtimePoint(response)
 		if err != nil {
 			glog.Fatal("Cannot write to InfluxDb. ", err)
